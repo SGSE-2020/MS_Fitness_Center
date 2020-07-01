3fd2ccffb1752b653331e611207af4894b52f3f8
Malte Riechmann <malte.riechmann@fh-bielefeld.de>
2020-07-02 00:36:01 +0200

    add rabbit publisher

---
+++ backend/src/rabbit_publisher.rb
@@ -0,0 +1,26 @@
+require 'bunny'
+
+def new_device(text)
+    result = 'success'
+    begin
+        connection = Bunny.new("amqp://testmanager:sgseistgeil@ms-rabbitmq:5672/")
+        connection.start
+
+        channel = connection.create_channel
+        exchange = channel.fanout("fitnesscenter", "fanout")
+        #severity = ARGV.shift || 'anonymous.info'
+        #message = ARGV.empty? ? 'Hello World!' : ARGV.join(' ')
+
+        #exchange.publish(message, routing_key: severity)
+        #puts " [x] Sent #{severity}:#{message}"
+
+        exchange.publish(text)
+        connection.close
+    rescue Bunny::Exception => e
+        result = e.message
+        puts e.message
+    ensure
+        connection.close if connection
+    end
+    result
+end
